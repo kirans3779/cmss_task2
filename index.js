e1d1c72886c0f1fe1e5b196d903fb679ded81e1d
Kiran Sonwane <77047645+kirans3779@users.noreply.github.com>
2022-05-30 13:29:29 +0530

    yes

--- index.js
+++ index.js
@@ -34,3 +34,397 @@
           }
         }
       });
+      let empdata= [
+        {
+        "name": "kiran sonwane",
+        "email":"kirans@3779@gmail.com",
+        "mobile":9960455716,
+        "empId":"emp1",
+        "img":"./imges/AbhirathNC.jpg",
+        "dept":"admin",
+        "designation":"Sr.Developer",
+    
+        },
+        {
+            "name": "suraj sonwane",
+            "email":"surajs@3779@gmail.com",
+            "mobile":9960455716,
+            "empId":"emp2",
+            "img":"./imges/AbhirathNC.jpg",
+            "dept":"admin",
+        "designation":"Sr.Developer",
+        
+            },
+             {
+                "name": "gaurav sonwane",
+                "email":"gauravs@3779@gmail.com",
+                "mobile":9960455716,
+                "empId":"emp3",
+                "img":"./imges/AbhirathNC.jpg",
+                "dept":"admin",
+                "designation":"Sr.Developer",
+            
+                },
+                {
+                  "name": "suraj sonwane",
+                  "email":"surajs@3779@gmail.com",
+                  "mobile":9960455716,
+                  "empId":"emp2",
+                  "img":"./imges/AbhirathNC.jpg",
+                  "dept":"admin",
+              "designation":"Sr.Developer",
+              
+                  },
+                  {
+                    "name": "suraj sonwane",
+                    "email":"surajs@3779@gmail.com",
+                    "mobile":9960455716,
+                    "empId":"emp2",
+                    "img":"./imges/AbhirathNC.jpg",
+                    "dept":"admin",
+                "designation":"Sr.Developer",
+                
+                    },
+                    {
+                      "name": "suraj sonwane",
+                      "email":"surajs@3779@gmail.com",
+                      "mobile":9960455716,
+                      "empId":"emp2",
+                      "img":"./imges/EkantSingh.jpg",
+                      "dept":"admin",
+                  "designation":"Sr.Developer",
+                  
+                      },
+                      {
+                        "name": "suraj sonwane",
+                        "email":"surajs@3779@gmail.com",
+                        "mobile":9960455716,
+                        "empId":"emp2",
+                        "img":"./imges/EkantSingh.jpg",
+                        "dept":"admin",
+                    "designation":"Sr.Developer",
+                    
+                        },
+                        {
+                          "name": "suraj sonwane",
+                          "email":"surajs@3779@gmail.com",
+                          "mobile":9960455716,
+                          "empId":"emp2",
+                          "img":"./imges/EkantSingh.jpg",
+                          "dept":"admin",
+                      "designation":"Sr.Developer",
+                      
+                          },
+                          {
+                            "name": "suraj sonwane",
+                            "email":"surajs@3779@gmail.com",
+                            "mobile":9960455716,
+                            "empId":"emp2",
+                            "img":"./imges/EkantSingh.jpg",
+                            "dept":"admin",
+                        "designation":"Sr.Developer",
+                        
+                            },
+                            {
+                              "name": "suraj sonwane",
+                              "email":"surajs@3779@gmail.com",
+                              "mobile":9960455716,
+                              "empId":"emp2",
+                              "img":"./imges/EkantSingh.jpg",
+                              "dept":"admin",
+                          "designation":"Sr.Developer",
+                          
+                              },
+                              {
+                                "name": "suraj sonwane",
+                                "email":"surajs@3779@gmail.com",
+                                "mobile":9960455716,
+                                "empId":"emp2",
+                                "img":"./imges/EkantSingh.jpg",
+                                "dept":"admin",
+                            "designation":"Sr.Developer",
+                            
+                                },
+                                {
+                                  "name": "suraj sonwane",
+                                  "email":"surajs@3779@gmail.com",
+                                  "mobile":9960455716,
+                                  "empId":"emp2",
+                                  "img":"./imges/EkantSingh.jpg",
+                                  "dept":"admin",
+                              "designation":"Sr.Developer",
+                              
+                                  },
+                                  {
+                                    "name": "suraj sonwane",
+                                    "email":"surajs@3779@gmail.com",
+                                    "mobile":9960455716,
+                                    "empId":"emp2",
+                                    "img":"./imges/EkantSingh.jpg",
+                                    "dept":"admin",
+                                "designation":"Sr.Developer",
+                                
+                                    },
+                                    {
+                                      "name": "suraj sonwane",
+                                      "email":"surajs@3779@gmail.com",
+                                      "mobile":9960455716,
+                                      "empId":"emp2",
+                                      "img":"./imges/EkantSingh.jpg",
+                                      "dept":"admin",
+                                  "designation":"Sr.Developer",
+                                  
+                                      },
+                                      {
+                                        "name": "suraj sonwane",
+                                        "email":"surajs@3779@gmail.com",
+                                        "mobile":9960455716,
+                                        "empId":"emp2",
+                                        "img":"./imges/EkantSingh.jpg",
+                                        "dept":"admin",
+                                    "designation":"Sr.Developer",
+                                    
+                                        },
+                                        {
+                                          "name": "suraj sonwane",
+                                          "email":"surajs@3779@gmail.com",
+                                          "mobile":9960455716,
+                                          "empId":"emp2",
+                                          "img":"./imges/EkantSingh.jpg",
+                                          "dept":"admin",
+                                      "designation":"Sr.Developer",
+                                      
+                                          },
+                                          {
+                                            "name": "suraj sonwane",
+                                            "email":"surajs@3779@gmail.com",
+                                            "mobile":9960455716,
+                                            "empId":"emp2",
+                                            "img":"./imges/EkantSingh.jpg",
+                                            "dept":"admin",
+                                        "designation":"Sr.Developer",
+                                        
+                                            },
+                                            {
+                                              "name": "suraj sonwane",
+                                              "email":"surajs@3779@gmail.com",
+                                              "mobile":9960455716,
+                                              "empId":"emp2",
+                                              "img":"./imges/EkantSingh.jpg",
+                                              "dept":"admin",
+                                          "designation":"Sr.Developer",
+                                          
+                                              },
+                                              {
+                                                "name": "suraj sonwane",
+                                                "email":"surajs@3779@gmail.com",
+                                                "mobile":9960455716,
+                                                "empId":"emp2",
+                                                "img":"./imges/EkantSingh.jpg",
+                                                "dept":"admin",
+                                            "designation":"Sr.Developer",
+                                            
+                                                },
+                                                {
+                                                  "name": "suraj sonwane",
+                                                  "email":"surajs@3779@gmail.com",
+                                                  "mobile":9960455716,
+                                                  "empId":"emp2",
+                                                  "img":"./imges/EkantSingh.jpg",
+                                                  "dept":"admin",
+                                              "designation":"Sr.Developer",
+                                              
+                                                  },
+                                                  {
+                                                    "name": "suraj sonwane",
+                                                    "email":"surajs@3779@gmail.com",
+                                                    "mobile":9960455716,
+                                                    "empId":"emp2",
+                                                    "img":"./imges/EkantSingh.jpg",
+                                                    "dept":"admin",
+                                                "designation":"Sr.Developer",
+                                                
+                                                    },
+                                                    {
+                                                      "name": "suraj sonwane",
+                                                      "email":"surajs@3779@gmail.com",
+                                                      "mobile":9960455716,
+                                                      "empId":"emp2",
+                                                      "img":"./imges/EkantSingh.jpg",
+                                                      "dept":"admin",
+                                                  "designation":"Sr.Developer",
+                                                  
+                                                      },
+                                                      {
+                                                        "name": "suraj sonwane",
+                                                        "email":"surajs@3779@gmail.com",
+                                                        "mobile":9960455716,
+                                                        "empId":"emp2",
+                                                        "img":"./imges/EkantSingh.jpg",
+                                                        "dept":"admin",
+                                                    "designation":"Sr.Developer",
+                                                    
+                                                        },
+                                                        {
+                                                          "name": "suraj sonwane",
+                                                          "email":"surajs@3779@gmail.com",
+                                                          "mobile":9960455716,
+                                                          "empId":"emp2",
+                                                          "img":"./imges/EkantSingh.jpg",
+                                                          "dept":"admin",
+                                                      "designation":"Sr.Developer",
+                                                      
+                                                          },
+                                                          {
+                                                            "name": "suraj sonwane",
+                                                            "email":"surajs@3779@gmail.com",
+                                                            "mobile":9960455716,
+                                                            "empId":"emp2",
+                                                            "img":"./imges/EkantSingh.jpg",
+                                                            "dept":"admin",
+                                                        "designation":"Sr.Developer",
+                                                        
+                                                            },
+                                                            {
+                                                              "name": "suraj sonwane",
+                                                              "email":"surajs@3779@gmail.com",
+                                                              "mobile":9960455716,
+                                                              "empId":"emp2",
+                                                              "img":"./imges/EkantSingh.jpg",
+                                                              "dept":"admin",
+                                                          "designation":"Sr.Developer",
+                                                          
+                                                              },
+                                                
+    ]
+    var state ={
+      'querySet':empdata,
+      'page':1,
+      'rows':6,
+    }
+    // search
+    
+
+    empDatahandle(empdata)
+   
+    
+
+    // console.log(state.page);
+    // pagination
+    function pagination(querySet,page,rows) {
+      var trimStart=(page-1)*rows
+      var trimEnd =trimStart+rows 
+      var trimData =querySet.slice(trimStart,trimEnd)
+
+      var pages =Math.ceil(querySet.length/rows)
+
+      return{
+        'querySet':trimData,
+        'pages':pages,
+      }
+    }
+    // pagination button
+    function pageButtons(pages) {
+      var wrapper=document.getElementById('pagination-wrapper')
+      
+      wrapper.innerHTML=''
+      var prev=(state.page-Math.floor(state.window/2))
+      var next=(state.page+Math.floor(state.window/2))
+      if (prev<1) {
+        prev=1
+        next=state.window
+      }if(next>pages){
+        prev-pages-(state.window-1)
+        next-pages
+        if(prev<1){
+          prev=1
+        }
+      }
+     
+      for(var page=1; page<=pages ; page++){
+  
+        wrapper.innerHTML+=` <li> <button value=${page} class="page btn btn-info">${page}</button></li>`
+  
+      }
+
+      if(state.page !=1){
+        wrapper.innerHTML =` <li> <button value=${1} class="page btn btn-info">First</button></li>`+ wrapper.innerHTML
+
+      }
+      if(state.page !=pages){
+        wrapper.innerHTML +=` <li> <button value=${pages} class="page btn btn-info">Last</button></li>`
+
+      }
+
+             $('.page').on('click',function() {
+               $("#company-table").empty()
+
+               console.log("working");
+               state.page=$(this).val();
+               empDatahandle()
+
+             });
+    }
+    // console.log(JSON.parse(empdata)[0].name );
+    function empDatahandle() {
+     var table =document.getElementById("company-table")
+
+    
+     var pagedata=pagination(state.querySet , state.page , state.rows)
+     
+     console.log('data:',pagedata);
+     
+     myList = pagedata.querySet
+     
+     console.log(myList);
+     
+    
+     for (let i = 0; i < myList.length; i++) {
+      
+       var row=` 
+       <tr>
+       <th scope="row">
+       <div class="user-profile">
+           <img src="${myList[i].img}" alt="profiles">
+       </div>
+   </th>
+       <td>
+           <p class="full-name">
+              ${myList[i].name}
+           </p>
+       </td>
+       <td>
+       <p class="dept">
+          ${myList[i].dept}
+       </p>
+   </td>
+        <td>
+           <p class="empId">
+               ${myList[i].empId}
+           </p>
+       </td>
+       <td>
+       <p class="email-Id">
+           ${myList[i].email}
+       </p>
+   </td>
+   <td>
+       <p class="email-Id">
+           ${myList[i].designation}
+       </p>
+   </td>
+       <td>
+           <p class="email">
+               ${myList[i].mobile}
+           </p>
+       </td>
+       
+     </tr>
+       `
+       table.innerHTML +=row
+       
+     }
+     pageButtons(pagedata.pages)
+    
+    }
+   
